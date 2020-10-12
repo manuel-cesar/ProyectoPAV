478580edce551b8abb735b146dc0e34b5f70f452
valentino tavella <valentinotavella@gmail.com>
2020-10-12 11:54:44 -0300

    Actualizar

--- ProductosYPlanes/Datos/Conexion/DBHelper.cs
+++ ProductosYPlanes/Datos/Conexion/DBHelper.cs
@@ -18,7 +18,7 @@ namespace ProductosYPlanes.Datos.Conexion
         {
             dbConnection = new SqlConnection();
 
-            var string_conexion = "Data Source=localhost;Initial Catalog=BugTracker_Extendida;Integrated Security=True";
+            var string_conexion = "Data Source=.\\SQLEXPRESS;Initial Catalog=bugs_extends;Integrated Security=True";
             dbConnection.ConnectionString = string_conexion;
         }
 
